aeeef6a18b6d52c71c8b1a612e7426afffb8bb2b
Dylan Rutter <dylan.rutter@my.wheaton.edu>
2017-10-02 18:04:34 -0500

    Update Deep learning.py
    added error functions and activation functions

--- Deep learning.py
+++ Deep learning.py
@@ -139,12 +139,94 @@ def _placeholder(dtype, shape=None, name=None):
    return result
 
 
-def softmax(x):
+def stepFunction(t):
     """
-    compute softmax values for x
+    perceptronStep helper function. takes a logit as input
     """
-    x = x * 10
-    return np.exp(x) / np.sum(np.exp(x), axis = 0)
+    if t >= 0:
+        return 1
+    return 0
+
+def prediction(X, W, b):
+    """
+    perceptronStep helper function. takes an data value, a weight, and a
+    bias as input
+    """
+    return stepFunction((np.matmul(X,W)+b)[0])
+
+
+def perceptronStep(X, y, W, b, learn_rate = 0.01):
+    """
+    updates weights and biases to help improve perceptron best fit line.
+    X is inputs of the data, y is the labels, W is the weights(as an array),
+    and bias is b. returns updated weight array and bias
+    """
+    for i in range(len(y)):
+        y_hat = prediction(X[i],W,b)
+        n = 0
+        if y[i]-y_hat == 1:
+            while n < len(W):
+                W[n] += X[i][n]*learn_rate
+                n+=1
+            b += learn_rate
+        elif y[i]-y_hat == -1:
+            while n < len(W):
+                W[n] -= X[i][n]*learn_rate
+                n+=1
+            b -= learn_rate
+    return W, b
+    
+
+def trainPerceptronAlgorithm(X, y, learn_rate = 0.01, num_epochs = 25):
+    """
+    runs perceptronStep repeatedly on a dataset. returns a few boundary
+    lines obtained in the iterations
+    """
+    x_min, x_max = min(X.T[0]), max(X.T[0])
+    y_min, y_max = min(X.T[1]), max(X.T[1])
+    W = np.array(np.random.rand(2,1))
+    b = np.random.rand(1)[0] + x_max
+
+    boundary_lines = []
+    for i in range(num_epochs):
+        W, b = perceptronStep(X, y, W, b, learn_rate)
+        boundary_lines.append((-W[0]/W[1], -b/W[1]))
+    return boundary_lines
+
+
+def softmax(logit_list):
+    """
+    compute softmax values for a list of logits
+    """
+    expL = np.exp(logit_list)
+    sumExpL = sum(logit_list)
+    result = []
+    for i in expL:
+        result.append(i*1.0/sumExpL)
+    return result
+
+def cross_entropy(Y, P):
+    """
+    computes cross_entropy for two lists
+    """
+    Y = np.float_(Y)
+    P = np.float_(P)
+    return -np.sum(Y * np.log(P) + (1 - Y) * np.log(1 - P))
+
+def sigmoid(x):
+    """
+    x is the summation of weight vector dot product with input vector added
+    to bias.
+    """
+    return 1/(1+np.exp(-x))
+
+def sigmoid_prime(x):
+    """
+    derivative of the sigmoid function
+    """
+    return sigmoid(x)*(1-sigmoid(x))
+
+
 
 keep_rate = 0.8
 
